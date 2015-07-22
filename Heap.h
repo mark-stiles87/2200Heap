5629c26fe84e75443151ca479ed1c744497048a6
mark-stiles87 <mark-stiles87@users.noreply.github.com>
2015-07-22 17:37:27 -0400

    Update Heap.h

--- Heap.h
+++ Heap.h
@@ -13,61 +13,63 @@
 
 using namespace std;
 
-template < typename KeyType=int >
+template < typename KeyType = int >
 class Less {
-  public:
-    bool operator()(const KeyType &a, const KeyType &b) const { return a < b; }
+public:
+	bool operator()(const KeyType &a, const KeyType &b) const { return a < b; }
 };
 
-template < typename DataType, typename KeyType=int, typename Comparator=Less<KeyType> >
+template < typename DataType, typename KeyType = int, typename Comparator = Less<KeyType> >
 class Heap
 {
-  public:
+public:
 
-    static const int DEFAULT_MAX_HEAP_SIZE = 10;    // Default maximum heap size
+	static const int DEFAULT_MAX_HEAP_SIZE = 10;    // Default maximum heap size
 
-    // Constructor
-    Heap (int = DEFAULT_MAX_HEAP_SIZE); // Default constructor + basic constr
-    Heap (const Heap&);		    // Copy constructor
-    Heap& operator= (const Heap&);  // Overloaded assignment operator
+	// Constructor
+	Heap(int = DEFAULT_MAX_HEAP_SIZE); // Default constructor + basic constr
+	Heap(const Heap&);		    // Copy constructor
+	Heap& operator= (const Heap&);  // Overloaded assignment operator
 
-    // Destructor
-    ~Heap ();
+	// Destructor
+	~Heap();
 
-    // Heap manipulation operations
-	void insert(const DataType& newDataItem);    // Insert a data item
-    DataType remove();  // Remove max priority element
-    void clear();                          // Clear heap
+	// Heap manipulation operations
+	void insert(const DataType&);    // Insert a data item
+	//Requires: A DataType to insert.
+	//Result: The item is inserted and the array is still in heap form.
+	DataType remove();  // Remove max priority element
+	void clear();                          // Clear heap
 
-    // Heap status operations
-    bool isEmpty() const;                  // Heap is empty
-    bool isFull() const;                   // Heap is full
+	// Heap status operations
+	bool isEmpty() const;                  // Heap is empty
+	bool isFull() const;                   // Heap is full
 
 	bool operator==(const Heap&) const; //overloaded boolean equality.
 	//Requires: Another heap.
 	//Result: Returns 1 if the heaps are equivalent or 0 otherwise.
 
-    // Output the heap structure -- used in testing/debugging
-    void showStructure() const;
+	// Output the heap structure -- used in testing/debugging
+	void showStructure() const;
 
-    // Programming exercise #3 operation
-    // void writeLevels() const;              // Output in level order
+	// Programming exercise #3 operation
+	// void writeLevels() const;              // Output in level order
 
-  private:
+private:
 
-    // Recursive helper of the showStructure() function
-    void showSubtree(int index, int level) const;
+	// Recursive helper of the showStructure() function
+	void showSubtree(int index, int level) const;
 
-	void heapifyUp(); //Helper for insertion.
+	void heapifyUp(const int, const DataType&); //Helper for insertion.
 	//Requires: an array that is not in heap form.
 	//Result: dataItems is in heap form.
 
-    // Data members
-    int maxSize,   // Maximum number of elements in the heap
-        size;      // Actual number of elements in the heap
-    DataType *dataItems; // Array containing the heap elements
+	// Data members
+	int maxSize,   // Maximum number of elements in the heap
+		size;      // Actual number of elements in the heap
+	DataType *dataItems; // Array containing the heap elements
 
-    Comparator comparator;
+	Comparator comparator;
 };
 
 template <typename DataType, typename KeyType, typename Comparator>
@@ -114,85 +116,92 @@ bool Heap<DataType, KeyType, Comparator>::operator==(const Heap& other) const
 template <typename DataType, typename KeyType, typename Comparator>
 Heap<DataType, KeyType, Comparator>::~Heap()
 {
-        clear();
-        delete[] dataItems;
+	clear();
+	delete[] dataItems;
+}
+
+template <typename DataType, typename KeyType, typename Comparator>
+void Heap<DataType, KeyType, Comparator>::insert(const DataType& newData)
+{
+	heapifyUp(size, newData);
+	size++;
+	return;
 }
 
 template <typename DataType, typename KeyType, typename Comparator>
 DataType Heap<DataType, KeyType, Comparator>::remove()
 {
-        if (size == 0)
-                return;
-        else if (size == 1)
-        {
-                retVal = new dataItems[0];
-                delete dataItems[0];
+	if (size == 0)
+		return;
+	else if (size == 1)
+	{
+		retVal = new dataItems[0];
+		delete dataItems[0];
 
-                return retVal;
-        else
-        {
-                const int last = size - 1;
-                const int first = 0;
+		return retVal;
+	}
+	else
+		const int last = size - 1;
+		const int first = 0;
 
-                retVal = new dataItems[first];
-                delete dataItems[first];
+		retVal = new dataItems[first];
+		delete dataItems[first];
 
-                dataItems[first] = new dataItems[last];
-                delete dataItems[last];
+		dataItems[first] = new dataItems[last];
+		delete dataItems[last];
 
-                int i = 0;
-                
-                // checks if the current data item is less than one of its children AND stops before end of array is
-                // reached
+		int i = 0;
 
-                while ((comparator(i, i+1) || comparator(i, i+2)) && comparator(i, size-3))
-                {
-                        if (comparator(i+2, i+1))
-                        {
-                                if (comparator(i, i+1))
-                                {
-                                        temp = new dataItems[i];
-                                        delete dataItems[i];
+		// checks if the current data item is less than one of its children AND stops before end of array is
+		// reached
 
-                                        dataItems[i] = new dataItems[i + 1];
-                                        dataItems[i + 1] = temp;
-                                        i = i + 1;
-                                }
-                        }
-                        else if (comparator(i+1, i+2))
-                        {
-                                if (comparator(i, i+2))
-                                {
-                                        temp = new dataItem[i];
-                                        delete dataItems[i];
+		while ((comparator(i, i + 1) || comparator(i, i + 2)) && comparator(i, size - 3))
+		{
+			if (comparator(i + 2, i + 1))
+			{
+				if (comparator(i, i + 1))
+				{
+					temp = new dataItems[i];
+					delete dataItems[i];
 
-                                        dataItems[i] = new dataItems[i + 2];
-                                        dataItems[i + 2] = temp;
-                                        i = i + 2;
-                                }
-                        }
-                        
-                }
-                return retVal;
-        }
+					dataItems[i] = new dataItems[i + 1];
+					dataItems[i + 1] = temp;
+					i = i + 1;
+				}
+			}
+			else if (comparator(i + 1, i + 2))
+			{
+				if (comparator(i, i + 2))
+				{
+					temp = new dataItem[i];
+					delete dataItems[i];
+
+					dataItems[i] = new dataItems[i + 2];
+					dataItems[i + 2] = temp;
+					i = i + 2;
+				}
+			}
+
+		}
+		return retVal;
 }
 
 template <typename DataType, typename KeyType, typename Comparator>
 Heap<DataType, KeyType, Comparator>::clear()
 {
-        for (int i = 0; i < size; i++)
-        {
-                if (dataItems[i] != NULL)
-                        delete dataItems[i];
-        }
+	for (int i = 0; i < size; i++)
+	{
+		if (dataItems[i] != NULL)
+			delete dataItems[i];
+	}
 }
 
 template < typename DataType, typename KeyType, typename Comparator >
 void Heap<DataType, KeyType, Comparator>::showStructure() const
 
-// Outputs the priorities of the data items in a heap in both array
-// and tree form. If the heap is empty, outputs "Empty heap". This
-// operation is intended for testing/debugging purposes only.
+	// Outputs the priorities of the data items in a heap in both array
+	// and tree form. If the heap is empty, outputs "Empty heap". This
+	// operation is intended for testing/debugging purposes only.
 
 {
 	int j;   // Loop counter
@@ -218,9 +227,9 @@ void Heap<DataType, KeyType, Comparator>::showStructure() const
 template < typename DataType, typename KeyType, typename Comparator >
 void Heap<DataType, KeyType, Comparator>::showSubtree(int index, int level) const
 
-// Helper function for the showStructure() function. Outputs the
-// subtree (subheap) whose root is stored in dataItems[index]. Argument
-// level is the level of this dataItems within the tree.
+	// Helper function for the showStructure() function. Outputs the
+	// subtree (subheap) whose root is stored in dataItems[index]. Argument
+	// level is the level of this dataItems within the tree.
 
 {
 	int j;   // Loop counter
@@ -238,4 +247,21 @@ void Heap<DataType, KeyType, Comparator>::showSubtree(int index, int level) cons
 		cout << endl;
 		showSubtree(2 * index + 1, level + 1);        // Output left subtree
 	}
+}
+
+template <typename DataType, typename KeyType, typename Comparator>
+void Heap<DataType, KeyType, Comparator>::heapifyUp(const int index, const DataType& data)
+{
+	int child = index, current = index;
+	int parent = (child - 1) / 2;
+	dataItems[index] = data;
+	while (comparator(dataItems[parent], data))
+	{
+		dataItems[child] = dataItems[parent];
+		child = parent;
+		parent = (child - 1) / 2;
+		current = child;
+	}
+	dataItems[current] = data;
+	return;
 }
