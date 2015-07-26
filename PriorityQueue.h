22f19be294f7ccc8a6c4f6e136680046ff192240
anthonymauro12 <en0435@wayne.edu>
2015-07-26 19:55:45 -0400

    Update PriorityQueue.h

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -20,26 +20,68 @@ using namespace std;
 
 const int MAX_QUEUE_SIZE = 10;   // Default maximum queue size
 
-template < typename DataType, typename KeyType=int, typename Comparator=Less<KeyType> >
+template < typename DataType, typename KeyType = int, typename Comparator = Less<KeyType> >
 class PriorityQueue : public Heap<DataType>
 {
-  public:
+public:
 
-    // Constructor
-    PriorityQueue ( int maxNumber = MAX_QUEUE_SIZE );
+	// Constructor
+	PriorityQueue(int maxNumber = MAX_QUEUE_SIZE);
 	// Destructor
 	~PriorityQueue();
 
+	PriorityQueue(const Heap& other);
 	PriorityQueue& operator = (const PriorityQueue& other);
 
-    // Queue manipulation operations
-    void enqueue ( const DataType &newDataItem );   // Enqueue data element
-    DataType dequeue ();  // Dequeue data element
-	
+	// Queue manipulation operations
+	void enqueue(const DataType &newDataItem);   // Enqueue data element
+	DataType dequeue();  // Dequeue data element
+
+
 	void clear();
 	bool isEmpty();
 	bool ifFull();
 
 };
+
+template <typename DataType, typename KeyType, typename Comparator>
+bool PriorityQueue<DataType, KeyType, Comparator>::isEmpty() const
+{
+	return 0 == size;
+}
+
+template <typename DataType, typename KeyType, typename Comparator>
+bool PriorityQueue<DataType, KeyType, Comparator>::isFull() const
+{
+	return size == maxSize;
+}
+
+template <typename DataType, typename KeyType, typename Comparator>
+PriorityQueue<DataType, KeyType, Comparator>::~PriorityQueue()
+{
+	clear();
+	delete[] dataItems;
+}
+
+template <typename DataType, typename KeyType, typename Comparator>
+PriorityQueue<DataType, KeyType, Comparator>::PriorityQueue(int maxNumber = MAX_QUEUE_SIZE)
+{
+	maxsize = maxNumber;
+	size = 0;
+	dataItems = new DataType[maxSize];
+}
+
+template <typename DataType, typename KeyType, typename Comparator>
+PriorityQueue<DataType, KeyType, Comparator>::PriorityQueue(const Heap& other)
+{
+	this = other;
+}
+
+template <typename DataType, typename KeyType, typename Comparator>
+void PriorityQueue<DataType, KeyType, Comparator>::clear()
+{
+	size = 0;
+	return;
+}
 
 #endif
